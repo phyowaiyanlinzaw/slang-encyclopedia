db632d946b54cd338947e5385cde61cec81d3edf
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-06-08 11:44:27 +0630

    Created OtpRequestDTO

--- src/main/java/dictionary/dao/OtpDAO.java
+++ src/main/java/dictionary/dao/OtpDAO.java
@@ -1,5 +1,21 @@
 package dictionary.dao;
 
-public class OtpDAO {
+import java.sql.Connection;
 
+import org.springframework.stereotype.Service;
+
+@Service("otpDao")
+public class OtpDAO {
+	
+	static Connection con=null;
+	
+	static {
+		con = MyConnection.getConnection();
+	}
+	
+	public int storeOtp() {
+		int result =0;
+		return result;
+	}
+	
 }
---
+++ src/main/java/dictionary/dto/OtpRequestDTO.java
@@ -0,0 +1,39 @@
+package dictionary.dto;
+
+public class OtpRequestDTO {
+	
+	private String otpNumber;
+	private String createdAt;
+	private String requestedBy;
+	private String expTime;
+	
+	public String getOtpNumber() {
+		return otpNumber;
+	}
+	public void setOtpNumber(String otpNumber) {
+		this.otpNumber = otpNumber;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getRequestedBy() {
+		return requestedBy;
+	}
+	public void setRequestedBy(String requestedBy) {
+		this.requestedBy = requestedBy;
+	}
+	public String getExpTime() {
+		return expTime;
+	}
+	public void setExpTime(String expTime) {
+		this.expTime = expTime;
+	}
+	
+	
+	
+	
+	
+}
