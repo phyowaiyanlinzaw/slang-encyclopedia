1312bf42271ae727b85954c5c26813154a7cb8a2
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-06-08 13:03:29 +0630

    Added storeOtp method inside OtpDao

--- src/main/java/dictionary/dao/OtpDAO.java
+++ src/main/java/dictionary/dao/OtpDAO.java
@@ -1,8 +1,16 @@
 package dictionary.dao;
 
+import java.sql.Timestamp;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 
 import org.springframework.stereotype.Service;
+
+import dictionary.dto.OtpRequestDTO;
 
 @Service("otpDao")
 public class OtpDAO {
@@ -13,8 +21,23 @@ public class OtpDAO {
 		con = MyConnection.getConnection();
 	}
 	
-	public int storeOtp() {
+	public int storeOtp(OtpRequestDTO req) {
+		
 		int result =0;
+		String sql = "insert into otp(otpNum,createdAt,createdBy,exp_time) values(?,?,?,?)";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, req.getOtpNumber());
+			ps.setDate(2, Date.valueOf(LocalDate.now()));
+			ps.setString(3, req.getRequestedBy());
+			ps.setTimestamp(4, Timestamp.valueOf(req.getExpTime()));
+			result = ps.executeUpdate();
+			
+		}catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
 		return result;
 	}
 	
