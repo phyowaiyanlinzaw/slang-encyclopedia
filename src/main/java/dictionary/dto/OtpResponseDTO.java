5f4bb8981684974a37564c9b666eeb2cc13b251d
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-06-08 13:39:24 +0630

    Created OtpResponseDTO

---
+++ src/main/java/dictionary/dto/OtpResponseDTO.java
@@ -0,0 +1,36 @@
+package dictionary.dto;
+
+public class OtpResponseDTO {
+	
+	private String otpNumber;
+	private String requestedBy;
+	private String expTime;
+	private String createdAt;
+	public String getOtpNumber() {
+		return otpNumber;
+	}
+	public void setOtpNumber(String otpNumber) {
+		this.otpNumber = otpNumber;
+	}
+	public String getRequestedBy() {
+		return requestedBy;
+	}
+	public void setRequestedBy(String requestedBy) {
+		this.requestedBy = requestedBy;
+	}
+	public String getExpTime() {
+		return expTime;
+	}
+	public void setExpTime(String expTime) {
+		this.expTime = expTime;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+
+}
