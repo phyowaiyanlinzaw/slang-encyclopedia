9b73d91390dcb50839d6f77d0d42b6db69290151
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-06-12 11:16:13 +0630

    User Profile Changes

--- src/main/java/dictionary/controller/DictionaryController.java
+++ src/main/java/dictionary/controller/DictionaryController.java
@@ -101,7 +101,8 @@ public class DictionaryController {
 	public ModelAndView otpView(
 			HttpSession session,
 			ModelMap m
-			) {
+			)
+			{
 		
 		String genereatedOtp = OtpService.generateOtp();
 		
@@ -169,6 +170,7 @@ public class DictionaryController {
 		}
 		
 		if(!isCorrectOTP) {
+			m.addAttribute("wrongOtp", "wrong");
 			return "redirect:/otpView";
 		}
 		
